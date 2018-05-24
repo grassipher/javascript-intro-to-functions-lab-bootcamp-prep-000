62a83aba461ea1a0a38f14574274ecf676a2b30a
Gabriel Valle <gvalle91@gmail.com>
2018-05-24 20:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toLowerCase === string){
       return "I can't hear you!"
-  } elseif (string.toUpperCase === string){
+  } else if (string.toUpperCase === string){
       return "YES INDEED!"
-  } elseif (string === "I love you, Grandma."){
+  } 
+  if (string === "I love you, Grandma."){
       return "I love you, too."
   }
 }
